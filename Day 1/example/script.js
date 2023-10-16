3741b5bdecde6ec21ffa269e257bcfccc2b5cf6b
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-16 14:41:37 +0100

    Complete day 1 task

--- Day 1/example/script.js
+++ Day 1/example/script.js
@@ -60,13 +60,3 @@ const listItems = document.querySelectorAll('ul>li:nth-child(odd)');
 console.log(listItems);
 
 title.innerText = "JavaScript Document Object Model";
-
-//!TASK
-/*
-in HTML page
-a h1 with id, three paragraphs, a h2, h3 with same classes.
-With JS:
-change the text of h1 to 'JavaScript DOM'
-change the text of h2, h3 to 'Text Changed using JS'
-change the text of all three paragraphs to whatever you want.
-*/
--- Day 1/task/script.js
+++ Day 1/task/script.js
@@ -7,3 +7,17 @@ change the text of h1 to 'JavaScript DOM'
 change the text of h2, h3 to 'Text Changed using JS'
 change the text of all three paragraphs to whatever you want.
 */
+
+document.getElementById('title').innerHTML = 'JavaScript DOM';
+
+const paragraphs = document.getElementsByTagName('p');
+
+for (paragraph of paragraphs) {
+  paragraph.innerHTML = 'This paragraph is cool now';
+}
+
+const headings = document.getElementsByClassName('small-title');
+
+for (heading of headings) {
+  heading.innerHTML = 'Text Changed using JS';
+}

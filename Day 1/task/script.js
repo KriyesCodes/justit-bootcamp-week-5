434a4196bd6b6fdcd9ecc9ee77441a07921ae1ef
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-16 17:02:40 +0100

    Complete 2nd task

--- Day 1/task/script.js
+++ Day 1/task/script.js
@@ -28,3 +28,22 @@ add a div in the HTML
 Create a h1 with text using JS and put it inside the div
 Change font size, text and background color to whatever you want
 */
+
+const div = document.querySelector('div');
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Title trapped in a box';
+div.append(h1);
+
+div.style.cssText = `
+background-color: dodgerblue;
+color: white;
+width: 200px;
+height: 200px;
+border: 10px solid black;
+`;
+
+h1.style.cssText = `
+font-size: 2rem;
+text-align: center;
+`;

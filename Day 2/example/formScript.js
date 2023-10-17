231ea7f258b9aabc149a1f1f925542c00a642d92
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-17 15:56:32 +0100

    Update example

--- Day 2/example/formScript.js
+++ Day 2/example/formScript.js
@@ -4,7 +4,6 @@ const text = document.getElementById('txtName');
 button.addEventListener('click', updateParagraph);
 
 function updateParagraph() {
-  console.log('click works');
   const p1 = document.getElementById('p1');
   p1.innerText = `Hey, how are you doing ${text.value}`;
 }

2c466324711a2e962119e9ef0e2cd7b4504ecec5
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-17 14:08:03 +0100

    Recap Day 1 learning

---
+++ Day 2/example/script.js
@@ -0,0 +1,26 @@
+/*
+What is DOM?
+  Document Object Model
+How to select an elemeent using it's Id?
+  method getElementById
+How can we select an element using it's class name?
+  method getElementsByClassName
+How can we select an element using tag?
+  method getElementsByTagName
+How can we change text of a HTML element?
+  innerText or textContent property
+What is the difference between querySelector and querySelectorAll methods?
+  querySelector selects the first element found of that query, querySelectorAll returns all found elements as a list
+How can we create a a new element?
+  createElement method
+How can we remove an element?
+  remove method
+How can we add class to a html element?
+  classList.add() method
+How can we remove a class from a html element?
+  classList.remove() method
+How can we add an attribute to a html element?
+  setAttribute method
+How can we remove an attribute of a html element?
+  removeAttribute method
+*/

3da78cf320644393ea4eda69b772bff14a35bfdd
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-18 14:42:33 +0100

    Complete day 3 task 2

--- Day 3/task/script.js
+++ Day 3/task/script.js
@@ -4,6 +4,10 @@
   when you click on the button it should save the data of
   text box in the local storage
   list data from local storage
+
+  task 2
+  each time when you click the save button:
+  save the text box data in the local storage without replacing the previous data
 */
 
 const textBox = document.getElementById('text-box');
@@ -14,9 +18,9 @@ submitButton.addEventListener('click', saveToLocalStorage);
 showDataButton.addEventListener('click', showLocalStorage);
 
 function saveToLocalStorage() {
-  localStorage.setItem('text', textBox.value);
+  localStorage.setItem(`${textBox.value}-KEY`, textBox.value);
 }
 
 function showLocalStorage() {
-  console.log(localStorage.getItem('text'));
+  console.log(localStorage);
 }

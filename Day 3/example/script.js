17889c0f291da88cbe1f98bdf5abaf74c07237ab
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-18 14:26:14 +0100

    Add local storage examples

--- Day 3/example/script.js
+++ Day 3/example/script.js
@@ -6,8 +6,23 @@ How can we access the value of a form input element?
 What is the difference between attached event in html and in js file?
   we can attach multiple events via js
 What is the syntax for addEventListener method?
-element.addEventListener('eventName', functionName);
+  element.addEventListener('eventName', functionName);
 write code which will attach an event to an element having an id='save'
-const saveButton = document.getElementById('save');
-saveButton.addEventListener('click', clickSave);
+  const saveButton = document.getElementById('save');
+  saveButton.addEventListener('click', clickSave);
 */
+
+// save the data in local storage
+localStorage.setItem('name', 'kriyes');
+
+// to get the data from local storage
+console.log(localStorage.getItem('name'));
+
+// to delete the data/key from local storage
+console.log(localStorage.removeItem('name'));
+
+// completely clear local storage
+localStorage.clear();
+
+// session storage - can be expired
+// local storage

63782f0b45caa5284828f9b68b7c7a75fcd7f4be
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-18 13:49:57 +0100

    Recape day 2 learning

---
+++ Day 3/example/script.js
@@ -0,0 +1,13 @@
+/*
+Which method is used to attach an event to a html element?
+  addEventListener();
+How can we access the value of a form input element?
+  value property of the input element
+What is the difference between attached event in html and in js file?
+  we can attach multiple events via js
+What is the syntax for addEventListener method?
+element.addEventListener('eventName', functionName);
+write code which will attach an event to an element having an id='save'
+const saveButton = document.getElementById('save');
+saveButton.addEventListener('click', clickSave);
+*/

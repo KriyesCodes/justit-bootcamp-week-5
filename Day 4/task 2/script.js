c4230dbc0083ce5c4018d0cb320d0e79eaad1dcc
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-19 16:24:36 +0100

    Complete task 2

--- Day 4/task 2/script.js
+++ Day 4/task 2/script.js
@@ -1,4 +1,8 @@
 // Thursday, 19th of October 2023
+// extension: Create a live digital clock (hh:mm:ss)
 
 let date1 = new Date(2023, 9, 19);
-console.log(date1);
+console.log(`${date1.toLocaleDateString('en-EN', { weekday: 'long' })}, ${date1.getDate()}th of ${date1.toLocaleDateString('en-EN', { month: 'long' })} ${date1.getFullYear()}`);
+
+
+

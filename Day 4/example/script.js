44426dee65e8ef8042f0317d8612b4f0c4626b09
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-10-19 16:02:28 +0100

    Add date object examples

--- Day 4/example/script.js
+++ Day 4/example/script.js
@@ -23,3 +23,33 @@ let repeat3 = setInterval(log, 3000, "repeat three");
 
 // to stop the above
 clearInterval(repeat3);
+
+
+// Date in JS
+
+let date = new Date();
+console.log(date);
+// let date1 = new Date(year, month, day, hour, minutes, seconds, milliseconds);
+// the first two we should pass
+let date2 = new Date(2023, 14, 19); //months indexed from 0, and value is converted with %12 so 14 becomes 2 which is March
+console.log(date2);
+
+let date3 = new Date().toLocaleString('en-US', { timeZone: 'Asia/Seoul' });
+console.log(date3);
+
+// toLocalDateString()
+
+let localDate = date.toLocaleDateString();
+console.log(localDate);
+
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getMonth());
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getSeconds());
+console.log(date.getTime()); //return time in milliseconds
+
+date.setMonth(11);
+date.setFullYear(2025);
+console.log(date);
